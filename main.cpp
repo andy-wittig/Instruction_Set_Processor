d9a8e393caf3138ae3bf029081f088a466e35534
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2025-03-27 19:31:59 -0700

    encapsulaton

--- main.cpp
+++ main.cpp
@@ -25,12 +25,67 @@ std::map<std::string, int> register_map //Assigns a register's string name to co
 };
 
 //Status Flag Init
-int n_flag;
-int z_flag;
-int c_flag;
-int v_flag;
+int n_flag = 0;
+int z_flag = 0;
+int c_flag = 0;
+int v_flag = 0;
+
+bool checkImmediate(std::string immediate)
+{
+	if (immediate[0] == '#')
+	{
+		uint32_t imm;
+		immediate.erase(0, 1);
+
+		try
+		{
+			if (immediate[0] == '0' && immediate[1] == 'x') //Check if value is hexidecimal or decimal.
+				imm = static_cast<std::uint32_t>(std::stoul(immediate, NULL, 16)); //Convert the string to an unsigned long and then cast to unsigned int of 32 bits.
+			else
+				imm = static_cast<std::uint32_t>(std::stoul(immediate));
+		}
+		catch (const std::exception& ex)
+		{
+			return false;
+		}
+
+		return true;
+	}
+	else return false;
+}
+
+bool checkRegister(std::string register_name) //Helper function to find if register exists.
+{
+	auto it = register_map.find(register_name);
+	if (it == register_map.end())
+	{
+		return false;
+	}
+	else return true;
+}
 
 //Validate Operator Functions
+bool validateTwoOperands(std::vector<std::string> operands, int line_number)
+{
+	if (operands.size() != 2) //Check for correct number of operands.
+	{
+		std::cerr << "Error: Invalid Operand Count on Line: " << std::to_string(line_number) << std::endl;
+		return false;
+	}
+
+	if (!checkRegister(operands[0]))
+	{
+		std::cerr << "ERROR ON LINE: " << std::to_string(line_number) << ", Invalid Operand(s)." << std::endl;
+		return false;
+	}
+
+	if (checkRegister(operands[1]) || checkImmediate(operands[1]))
+	{
+		return true;
+	}
+	else return false;
+}
+
 bool validateArithmeticOperands(std::vector<std::string> operands, int operand_count, int line_number)
 {
 	if (operands.size() != operand_count) //Check for correct number of operands.
@@ -39,61 +94,121 @@ bool validateArithmeticOperands(std::vector<std::string> operands, int operand_c
 		return false;
 	}
 
-	auto it = register_map.find(operands[0]); //Check if destination OPCODE is not a valid register.
-	if (it == register_map.end())
+	if (!checkRegister(operands[0]) || !checkRegister(operands[1]))
 	{
-		std::cerr << "ERROR ON LINE: " << std::to_string(line_number) << ", Inavlid Destination Register." << std::endl;
+		std::cerr << "ERROR ON LINE: " << std::to_string(line_number) << ", Invalid Operand(s)." << std::endl;
 		return false;
 	}
 
-	it = register_map.find(operands[1]); //Check if OPCODE 2 is not a valid register.
-	if (it == register_map.end())
-	{
-		std::cerr << "ERROR ON LINE: " << std::to_string(line_number) << ", Operand 2 Must be Valid Register." << std::endl;
-		return false;
-	}
-
-	it = register_map.find(operands[2]); //Check OPCODE 3 is valid register.
-	if (it != register_map.end())
+	if (checkRegister(operands[2]) || checkImmediate(operands[2]))
 	{
 		return true;
 	}
-	else if (operands[2][0] == '#')
+	else return false;
+}
+
+void processCMP(std::vector<std::string> operands)
+{
+	uint32_t operand1;
+	uint32_t operand2;
+
+	operand1 = register_array[register_map[operands[0]]];
+
+	if (operands[1][0] == '#') //OPCODE is an immediate.
 	{
-		uint32_t imm;
+		operands[1].erase(0, 1);
+
+		if (operands[1][0] == '0' && operands[1][1] == 'x') //Check if value is hexidecimal or decimal.
+			operand2 = static_cast<std::uint32_t>(std::stoul(operands[1], NULL, 16)); //Convert the string to an unsigned long and then cast to unsigned int of 32 bits.
+		else
+			operand2 = static_cast<std::uint32_t>(std::stoul(operands[1]));
+	}
+	else //OPCODE is a register
+	{
+		operand2 = register_array[register_map[operands[1]]];
+	}
+
+	n_flag = sign()
+
+
+}
+
+uint32_t processMov(std::vector<std::string> operands)
+{
+	uint32_t operand1;
+
+	if (operands[1][0] == '#') //OPCODE is an immediate.
+	{
+		operands[1].erase(0, 1);
+
+		if (operands[1][0] == '0' && operands[1][1] == 'x') //Check if value is hexidecimal or decimal.
+			operand1 = static_cast<std::uint32_t>(std::stoul(operands[1], NULL, 16)); //Convert the string to an unsigned long and then cast to unsigned int of 32 bits.
+		else
+			operand1 = static_cast<std::uint32_t>(std::stoul(operands[1]));
+	}
+	else //OPCODE is a register
+	{
+		operand1 = register_array[register_map[operands[1]]];
+	}
+
+	return operand1;
+}
+
+uint32_t processArithmetic(std::vector<std::string> operands, std::string operation_type)
+{
+	uint32_t operand2;
+	uint32_t operand3;
+
+	operand2 = register_array[register_map[operands[1]]];
+
+	if (operands[2][0] == '#') //OPCODE is an immediate.
+	{
 		operands[2].erase(0, 1);
 
-		try
-		{
-			if (operands[2][0] == '0' && operands[2][1] == 'x') //Check if value is hexidecimal or decimal.
-				imm = static_cast<std::uint32_t>(std::stoul(operands[2], NULL, 16)); //Convert the string to an unsigned long and then cast to unsigned int of 32 bits.
-			else
-				imm = static_cast<std::uint32_t>(std::stoul(operands[2]));
-		}
-		catch (const std::exception& ex)
-		{
-			std::cerr << "ERROR ON LINE: " << std::to_string(line_number) << ", Operand 3 is Invalid: " << ex.what() << std::endl;
-			return false;
-		}
+		if (operands[2][0] == '0' && operands[2][1] == 'x') //Check if value is hexidecimal or decimal.
+			operand3 = static_cast<std::uint32_t>(std::stoul(operands[2], NULL, 16)); //Convert the string to an unsigned long and then cast to unsigned int of 32 bits.
+		else
+			operand3 = static_cast<std::uint32_t>(std::stoul(operands[2]));
 	}
-	else
+	else //OPCODE is a register
 	{
-		std::cerr << "ERROR ON LINE: " << std::to_string(line_number) << "Operand 3 is Invalid." << std::endl;
-		return false;
+		operand3 = register_array[register_map[operands[2]]];
 	}
-	return true;
+
+	if (operation_type == "ADD")
+		return operand2 + operand3;
+	if (operation_type == "SUB")
+		return operand2 - operand3;
+	if (operation_type == "AND")
+		return operand2 & operand3;
+	if (operation_type == "OR")
+		return operand2 | operand3;
+	if (operation_type == "XOR")
+		return operand2 ^ operand3;
 }
 
 void displayRegistersAndMemory()
 {
 	int register_size = sizeof(register_array) / sizeof(register_array[0]);
+	int memory_size = sizeof(memory_array) / sizeof(memory_array[0]);
 
 	std::cout << "Register Array:" << std::endl;
 	for (int i = 0; i < register_size; i++)
 	{
-		std::cout << "R" << i << "=" << register_array[i] << ", ";
+		std::cout << "R" << i << "=0x" << std::hex << std::uppercase << register_array[i] << ", ";
 	}
-	std::cout << "\nMemory Array:" << std::endl;
+	std::cout << "\n";
+	std::cout << "Memory Array:" << std::endl;
+	for (int i = 0; i < memory_size; i++)
+	{
+		std::cout << memory_array[i] << ", ";
+	}
+	std::cout << "\n";
+}
+
+void displayStatusFlags()
+{
+	std::cout << "N: " << n_flag << ", Z: " << z_flag << ", C: " << c_flag << ", V: " << v_flag << std::endl;
 }
 
 int main()
@@ -115,10 +230,12 @@ int main()
 			std::string operand;
 			std::vector<std::string> operand_vec;
 
+			std::cout << "--> " << line << std::endl;
+
 			ss >> operation;
 
 			//Arithmetic Operations
-			if (operation == "ADD")
+			if (operation == "ADD" || operation == "SUB" || operation == "AND" || operation == "OR" || operation == "XOR")
 			{
 				while (ss >> operand) //Seperate tokens by whitespace
 				{
@@ -128,28 +245,37 @@ int main()
 
 				if (validateArithmeticOperands(operand_vec, 3, line_number))
 				{
-					uint32_t operand2;
-					uint32_t operand3;
-
-					operand2 = register_array[register_map[operand_vec[1]]];
-
-					if (operand_vec[2][0] == '#') //OPCODE is an immediate.
-					{
-						operand_vec[2].erase(0, 1);
-
-						if (operand_vec[2][0] == '0' && operand_vec[2][1] == 'x') //Check if value is hexidecimal or decimal.
-							operand3 = static_cast<std::uint32_t>(std::stoul(operand_vec[2], NULL, 16)); //Convert the string to an unsigned long and then cast to unsigned int of 32 bits.
-						else
-							operand3 = static_cast<std::uint32_t>(std::stoul(operand_vec[2])); 
-					}
-					else //OPCODE is a register
-					{
-						operand3 = register_array[register_map[operand_vec[2]]];
-					}
-
-					register_array[register_map[operand_vec[0]]] = operand2 + operand3;
-
+					register_array[register_map[operand_vec[0]]] = processArithmetic(operand_vec, operation);
 					displayRegistersAndMemory();
+				}
+			}
+			else if (operation == "MOV")
+			{
+				while (ss >> operand)
+				{
+					operand.erase(std::remove(operand.begin(), operand.end(), ','), operand.end());
+					operand_vec.push_back(operand);
+				}
+
+				if (validateTwoOperands(operand_vec, line_number))
+				{
+					register_array[register_map[operand_vec[0]]] = processMov(operand_vec);
+					displayRegistersAndMemory();
+				}
+			}
+			else if (operation == "CMP")
+			{
+				while (ss >> operand)
+				{
+					operand.erase(std::remove(operand.begin(), operand.end(), ','), operand.end());
+					operand_vec.push_back(operand);
+				}
+
+				if (validateTwoOperands(operand_vec, line_number))
+				{
+					displayRegistersAndMemory();
+					processCMP(operand_vec);
+					displayStatusFlags();
 				}
 			}
 			else
